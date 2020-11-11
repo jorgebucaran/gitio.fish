7d083544d835e4d2d3a1240a05fc282b58829748
Jorge Bucaran <mail@jorgebucaran.com>
2020-11-12 03:50:37 +0900

    Rewrite for fish 3.0

--- gitio.fish
+++ gitio.fish
@@ -1,66 +1,59 @@
 set -g gitio_version 1.0.1
 
-function gitio -d "Create a Git.io URL (git.io/<code>)"
+function gitio -d "Create a git.io URL"
     switch "$argv"
-        case {,-}-v{ersion,}
+        case -v --version
             echo gitio version $gitio_version
-        case {,-}-h{elp,} ""
-            echo "usage: gitio [<code>=]<url>   Create a Git.io URL (git.io/<code>)"
-            echo "       gitio --help           Display this help"
-            echo "       gitio --version        Display the current version"
+        case "" -h --help
+            echo "usage: gitio <url>        create a random git.io URL"
+            echo "       gitio <key>=<url>  create git.io/<key>"
+            echo "options:"
+            echo "       -v or --version  print gitio version"
+            echo "       -h or --help     print this help message"
             echo "examples:"
             echo "       gitio https://github.com/user/repo"
-            echo "       gitio repo=https://github.com/user/repo"
-            echo "       gitio ball=https://codeload.github.com/user/repo/tar.gz/master"
-            echo "       gitio boot=https://raw.githubusercontent.com/user/repo/v2/boot"
+            echo "       gitio gitio=https://github.com/jorgebucaran/gitio.fish"
         case \*
             if set -q argv[2]
-                echo "gitio: invalid argument count -- use \"gitio code=url\"" >&2
+                echo "gitio: too many arguments (see `gitio -h`)" >&2
                 return 1
             end
 
-            command awk -v s="$argv" '
-                BEGIN {
-                    url = (s ~ /^https:\/\/.+/) ? s : (i = index(s, "=")) ? substr(s, i + 1) : s
-                    if (i && !(code = substr(s, 1, i - 1))) exit
+            set argv (string split = "$argv")
+            set -l url $argv[2]
+            set -q argv[2] && set -l key $argv[1] || set url $argv[1]
 
-                    is_valid_code = !code || (length(code) <= 50 && code ~ /^[A-Za-z0-9.,+!*\'()$@_-]+$/)
-                    is_valid_url = url ~ /^https:\/\/(github|(gist|raw)\.githubusercontent|(gist|codeload)\.github)\.com\/[^ \t]+$/
-
-                    if (is_valid_url && is_valid_code) print url,code
-                }
-            ' | read -l url code
-
-            if not set -q url[1]
-                echo "gitio: invalid URL or code \"$argv\"" >&2
+            if set -q key[1] && not string match --quiet --regex '^[A-Za-z0-9.,+!*$@_-]+$' $key
+                echo "gitio: invalid git.io key: \"$key\"" >&2
                 return 1
             end
 
-            set -l opts -si https://git.io --data-urlencode "url=$url"
-            if test ! -z "$code"
-                set opts $opts --data-urlencode code="$code"
+            if not string match --quiet --regex '^https:\/\/(github|(gist|raw)\.githubusercontent|(gist|keyload)\.github)\.com\/[^ \t]+$' $url
+                echo "gitio: invalid URL: \"$url\"" >&2
+                return 1 
             end
 
-            set -l short (command curl $opts | command awk -v code="$code" -v error=3 '
-                /^HTTP\/1\.1 422/ { exit error = 2 }
-                /^HTTP\/1\.1 503/ { exit error = 3 }
-                /^Location:/ {
-                    print gsub("\r", "", $2) ? $2 : $2
-                    exit (error = code && "https://git.io/"code != $2)
-                }
-                END { exit error }
-            ')
-            or begin
-                switch $status
-                    case 1
-                        echo "gitio: $url already has a short URL \"$short\"" >&2
-                    case 2
-                        echo "gitio: https://git.io/$code is not available -- try a different code" >&2
-                    case \*
-                        echo "gitio: service unavailable -- are you offline?" >&2
-                end
+            set -l opts -si https://git.io --data-urlenkey "url=$url"
+            set -q key[1] && set opts $opts --data-urlenkey key="$key"
+            set -l resp (command curl $opts | string collect)
+
+            if string match --quiet --regex '^HTTP/1\.1 422' $resp
+                echo "gitio: key already used: \"$key\" -- try a new one" >&2
                 return 1
             end
-            echo $short
+
+            set -l short (string replace --all \r "" $resp | string match --regex 'Location: ([^ ]+)')
+
+            if test -z "$short[2]"
+                echo "gitio: host unavailable" >&2
+                return 1
+            end
+            
+            if set -q key[1] && test "$short[2]" != "https://git.io/$key"
+                echo "gitio: \"$url\" already using: \"$short[2]\"" >&2
+                return 1     
+            end
+
+            echo $short[2]
     end
 end
